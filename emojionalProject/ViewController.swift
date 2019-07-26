c7e99dec4db2619bfd6cd842d03a4f5128f66a67
Lili <educationapplication2024@gmail.com>
2019-07-26 10:06:56 -0400

    Iteration 0

---
+++ emojionalProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  emojionalProject
+//
+//  Created by Lili on 7/26/19.
+//  Copyright © 2019 Lili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func clickButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func clickButton2(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
